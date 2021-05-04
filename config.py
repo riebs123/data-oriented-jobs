61b0adfae77343ceedf7174fdfcc57de7c5187a7
riebs123 <tannerriebel@hotmail.com>
2021-05-04 17:27:18 -0500

    added database connection and config.py file

---
+++ config.py
@@ -0,0 +1,3 @@
+username = 'postgres'  # Ideally this would come from config.py (or similar)
+password = 'asdf'  # Ideally this would come from config.py (or similar)
+db = 'DataJobs' # Created in Week 9, Night 1, Exercise 08-Stu_CRUD 
