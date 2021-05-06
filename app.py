909f57262fd8481718b6d8178f19f528c03dd485
MMidwinter <75645693+MMidwinter@users.noreply.github.com>
2021-05-06 18:31:02 -0500

    Added super dictionary

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ base = automap_base()
 base.prepare(engine, reflect=True)
 
 # Choose the table we wish to use
-table = base.classes.combined
+table = base.classes.data_jobs
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 
@@ -32,7 +32,44 @@ def IndexRoute():
     webpage = render_template("index.html")
     return webpage
 
-@app.route("/salaryvscompanyrating")
+@app.route("/superdict")
+def QuerySuperDict():
+
+    #open a session, run the query, then close it again
+    session = session(engine)
+    results = session.query(table.job_category, table.job_title, table.salary_estimate, table.job_description, table.rating, table.company_name, table.location, table.headquarters, table.size, table.type_of_ownership, table.industry, table.sector, table.revenue, table.competitors, table.easy_apply)
+    session.close
+
+    super_dictionary = []
+    for job_category, job_title, salary_estimate, job_description, rating, company_name, location, headquarters, size, type_of_ownership, industry, sector, revenue, competitors, easy_apply in results:
+        dict = {}
+        dict["job_category"] = job_category
+        dict["job_title"] = job_title
+        dict["salary_estimate"] = salary_estimate
+        dict["job_description"] = job_description
+        dict["rating"] = rating
+        dict["company_name"] = company_name
+        dict["location"] = location
+        dict["headquarters"] = headquarters
+        dict["size"] = size
+        dict["type_of_ownership"] = type_of_ownership
+        dict["industry"] = industry
+        dict["sector"] = sector
+        dict["revenue"] = revenue
+        dict["competitors"] = competitors
+        dict["easy_apply"] = easy_apply
+        super_dictionary.append(dict)
+
+        return jsonify(super_dictionary)
+        
+
+@app.route('/salaryscatter')
+def QuerySalaryScatter():
+
+    session = session(engine)
+    results = session.query(table.)
+
+#@app.route("/salaryvscompanyrating")
 
 if __name__ == '__main__':
     app.run(debug=True)
